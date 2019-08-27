746a553a4c535333b370d5547835b057ff690e02
pooya haratian <pooyaharatian@gmail.com>
2019-08-27 18:27:30 +0430

    prod extra arguements

--- prod.sh
+++ prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export DJANGO_DEBUG=False
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build "$@"
