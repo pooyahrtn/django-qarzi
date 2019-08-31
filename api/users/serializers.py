e5941ab85b344f3eb6e5fd3d50638759c7988798
pooya haratian <pooyaharatian@gmail.com>
2019-08-31 18:38:44 +0430

    active bug in serializer

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -13,7 +13,7 @@ class UserSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'image', 'notification_token', 'active')
+        fields = ('id', 'username', 'first_name', 'last_name', 'image', 'notification_token', 'is_active')
         read_only_fields = ('username', 'id')
 
 
